7a67e1424401351485d4475ee7c1b5521f9fe7dd
Milan Lesichkov <lesichkovm@gmail.com>
2019-09-10 22:48:04 +0100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,14 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+const port = Deno.env().PORT || 8080;
+const s = serve(`:${port}`);
+
+async function main() {
+  console.info(`Listening on port ${port}`);
+  for await (const req of s) {
+    console.info(`New request: ${req.url}`);
+    req.respond({ body: new TextEncoder().encode(`You are now at ${req.url}\n`) });
+  }
+}
+
+main();
